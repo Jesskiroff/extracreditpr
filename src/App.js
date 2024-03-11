ed4b01706c76688555ef4c09e8adbc19bf4b62ce
Jessica Kiroff <jesskiroff@gmail.com>
2024-03-10 23:17:36 -0400

    add useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
+
+
+
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className='App'>
-      <h1>Hello react</h1>
+
+      <button> - </button>
+      <h1>{counter}</h1>
+      <button> + </button>
 
     </div>
   );
