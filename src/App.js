302319af3306e9cf7cd8b01235539e4773342bb1
Jessica Kiroff <jesskiroff@gmail.com>
2024-03-10 23:23:50 -0400

    add setCounter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import { useState } from 'react';
 import './App.css';
 
-
-
-
 const App = () => {
   const [counter, setCounter] = useState(0);
 
   return (
     <div className='App'>
 
-      <button onClick={() => alert('clicked')}>- </button>
+      <button onClick={() => setCounter((prevCount)=> prevCount -1 )}>- </button>
       <h1>{counter}</h1>
-      <button> + </button>
+      <button onClick={() => setCounter((prevCount)=> prevCount +1 )}>+ </button>
 
     </div>
   );
